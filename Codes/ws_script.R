e08242282d6926610154ce1ef55e3a191156f9a9
BJBerauer <bernd.berauer@gmx.de>
2020-01-15 17:12:44 +0000

    Really basic (and super brief) 1st contact with R introduction added

--- Codes/ws_script.R
+++ Codes/ws_script.R
@@ -3,3 +3,37 @@
 ###           -WORKSHOP SCRIPT 2020-
 ### BonaRes-R-Group | Authors
 ###################################################
+
+library(tidyverse) #load in the tidyverse (loading multiple packages belonging to the "tidyverse")
+
+###
+# Hooking people up and bringing people to the same level
+# ??Very much too basic??
+###
+
+#Use R as calculator
+2-4
+
+4/2+4 
+
+4/(2+4) #follows mathematical rules
+
+2*4+4/pi #"pi" is a prestored object - what are objects?
+
+#Create objects in R
+a <- 2
+b <- 4
+
+a-b
+b/a+b
+b/(a+b)
+a*b+b/pi
+
+d <- c(2,4,6) #vector - chain of multiple "entries"
+e <- c(2,5,8,11)
+d+e
+
+f <- c("can","be","letters")
+
+# Load in Data
+# !Starting from here might be ok as well
